c1fc1e087bccc03c8230dd4bed8eeab65a5e8749
Gavin-John Noonan <gavinjohn.noonan@gmail.com>
2015-03-19 20:42:38 +0000

    initial checkin

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mailgun-clj "0.1.0-SNAPSHOT"
+  :description "A Clojure wrapper around the mailgun api"
+  :url "https://github.com/adulteratedjedi/mailgun-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
